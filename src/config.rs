a04f4b572804e6cd6602904490116671857b776c
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-31 15:55:18 -0400

    Add file to handle loading config (such as .env)

---
+++ src/config.rs
@@ -0,0 +1,35 @@
+use anyhow::Result;
+use dotenv::dotenv;
+use std::env::var;
+
+#[derive(Clone)]
+pub struct Config {
+    pub env: Env,
+}
+
+#[derive(Clone)]
+pub struct Env {
+    pub token: String,
+    pub owner_id: String,
+    pub application_id: u64,
+    pub test_server_id: u64,
+}
+
+impl Config {
+    pub fn load() -> Result<Self> {
+        Ok(Self { env: Env::load()? })
+    }
+}
+
+impl Env {
+    pub fn load() -> Result<Self> {
+        dotenv().ok();
+
+        Ok(Self {
+            token: var("TOKEN")?,
+            owner_id: var("OWNER_ID")?,
+            application_id: var("APPLICATION_ID")?.parse()?,
+            test_server_id: var("TEST_SERVER_ID")?.parse()?,
+        })
+    }
+}
