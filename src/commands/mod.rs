b293049ac9709a86226e0f6c9ba1fba62d53636c
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-31 15:57:36 -0400

    Register commands here instead of main file

---
+++ src/commands/mod.rs
@@ -0,0 +1,14 @@
+mod meta;
+
+pub use super::*;
+
+use anyhow::{Error, Result};
+
+pub fn command_list(
+    mut options: poise::FrameworkOptions<State, Error>,
+) -> Result<poise::FrameworkOptions<State, Error>> {
+    options.command(meta::info(), |f| f.category("Meta"));
+    options.command(meta::help(), |f| f.category("Meta"));
+
+    Ok(options)
+}
