48cffdd3d8589c449e8452bb666095c71a6fd7fb
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-08-31 22:37:14 -0400

    Add a test dropdown menu to help command (will be functional later)

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -12,7 +12,29 @@ pub async fn info(ctx: Context<'_>) -> Result<()> {
 /// Shows all of the bots commands
 #[poise::command(slash_command)]
 pub async fn help(ctx: Context<'_>) -> Result<()> {
-    poise::send_reply(ctx, |m| m.embed(|e| e.description("help"))).await?;
+    poise::send_reply(ctx, |m| {
+        m.content("this is a test".to_string());
+        m.components(|c| {
+            c.create_action_row(|ar| {
+                ar.create_select_menu(|sm| {
+                    sm.placeholder("testing");
+                    sm.min_values(1);
+                    sm.max_values(1);
+                    sm.custom_id("testing");
+
+                    sm.options(|o| {
+                        o.create_option(|o| {
+                            o.label("test");
+                            o.value("0");
+                            o.description("this is a test");
+                            o
+                        })
+                    })
+                })
+            })
+        })
+    })
+    .await?;
 
     let mut categories: Vec<(Option<&str>, Vec<&poise::PrefixCommand<_, _>>)> = Vec::new();
     for cmd_meta in &ctx.framework().options().prefix_options.commands {
