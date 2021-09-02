02171ef981a71564761a58c9b492544a59aee03c
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-09-02 13:07:37 -0400

    Correctly load default embed color

--- src/config.rs
+++ src/config.rs
@@ -13,7 +13,7 @@ pub struct Env {
     pub owner_id: String,
     pub application_id: u64,
     pub prefix: String,
-    pub default_embed_color: u64,
+    pub default_embed_color: serenity::utils::Colour,
     pub test_server_id: u64,
 }
 
@@ -27,12 +27,26 @@ impl Env {
     pub fn load() -> Result<Self> {
         dotenv().ok();
 
+        let default_embed_color: Vec<u8> = var("DEFAULT_EMBED_COLOR")?
+            .replace("(", "")
+            .replace(")", "")
+            .replace(" ", "")
+            .split(",")
+            .collect::<Vec<&str>>()
+            .iter()
+            .map(|c| -> u8 { c.parse::<u8>().unwrap() })
+            .collect();
+
         Ok(Self {
             token: var("TOKEN")?,
             owner_id: var("OWNER_ID")?,
             application_id: var("APPLICATION_ID")?.parse()?,
             prefix: var("PREFIX")?,
-            default_embed_color: var("DEFAULT_EMBED_COLOR")?.parse()?,
+            default_embed_color: serenity::utils::Colour::from_rgb(
+                *default_embed_color.get(0).unwrap(),
+                *default_embed_color.get(2).unwrap(),
+                *default_embed_color.get(2).unwrap(),
+            ),
             test_server_id: var("TEST_SERVER_ID")?.parse()?,
         })
     }
