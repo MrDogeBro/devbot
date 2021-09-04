547da30ed31bd03919449bedcd8f9297a5d212a0
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-09-04 17:42:31 -0400

    Add utils folder with chron utils

---
+++ src/utils/chron.rs
@@ -0,0 +1,31 @@
+use anyhow::Result;
+use chrono::{prelude::Utc, DateTime, Duration};
+
+pub fn time_diff(start: DateTime<Utc>, end: DateTime<Utc>) -> Result<String> {
+    let duration: Duration = end - start;
+    let mut diff: Vec<String> = vec![];
+
+    let weeks = duration.num_weeks();
+    let days = duration.num_days() - (weeks * 7);
+    let hours = duration.num_hours() - (days * 24);
+    let mins = duration.num_minutes() - (hours * 60);
+    let seconds = duration.num_seconds() - (mins * 60);
+
+    if weeks > 0 {
+        diff.push(format!("{}w", weeks));
+    }
+    if days > 0 {
+        diff.push(format!("{}d", days));
+    }
+    if hours > 0 {
+        diff.push(format!("{}h", hours));
+    }
+    if mins > 0 {
+        diff.push(format!("{}m", mins));
+    }
+    if seconds > 0 {
+        diff.push(format!("{}s", seconds));
+    }
+
+    Ok(diff.join(", "))
+}
