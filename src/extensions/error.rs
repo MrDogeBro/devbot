b9b0f80508a4a08e2f07cabe3369626fa1894bd4
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-12-20 17:28:05 -0500

    Add processing for builtin errors (not custom)

--- src/extensions/error.rs
+++ src/extensions/error.rs
@@ -9,12 +9,24 @@ pub static ERRORS: Map<&'static str, &'static str> = phf_map! {
     "BOT_PERMS_ERROR" => "The bot does not have the {} permission required to execute this command!",
 };
 
+pub static BUILTIN_ERRORS: Map<&'static str, &'static str> = phf_map! {
+    "Failed to parse argument: Member not found or unknown format" => "Invalid member argument provided! Make sure they are a member of the current guild.",
+};
+
 pub async fn handle_error(err: Error) -> Result<String> {
     let err_msg = ERRORS.get(err.to_string().as_str());
 
     if let Some(err_msg) = err_msg {
         if let Some(err_ctx) = err.chain().skip(1).next() {
             return Ok(err_msg.replace("{}", err_ctx.to_string().as_str()));
+        }
+    }
+
+    for e in BUILTIN_ERRORS.keys() {
+        if err.to_string().starts_with(e) {
+            if let Some(builtin_err_msg) = BUILTIN_ERRORS.get(e) {
+                return Ok(builtin_err_msg.to_string());
+            }
         }
     }
 
